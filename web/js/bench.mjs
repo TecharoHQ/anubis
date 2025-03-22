9174172b6d199d8a0f01d3283fccb146e0ac9739
jae beller <foss@jae.zone>
2025-03-21 21:37:23 -0400

    cmd/anubis: add a debug option for benchmarking hashrate
    Having the ability to benchmark different proof-of-work implementations
    is useful for extending Anubis. This adds a flag `--debug-benchmark-js`
    (and its associated environment variable `DEBUG_BENCHMARK_JS`) for
    serving a tool to do so.
    
    Internally, a there is a new policy action, "DEBUG_BENCHMARK", which
    serves the benchmarking tool instead of a challenge. The flag then
    replaces all bot rules with a special rule matching every request
    to that action. The benchmark page makes heavy use of inline styles,
    because currently all global styles are shared across all pages. This
    could be fixed, but I wanted to avoid major changes to the templates.

---
+++ web/js/bench.mjs
@@ -0,0 +1,106 @@
+import processFast from "./proof-of-work.mjs";
+import processSlow from "./proof-of-work-slow.mjs";
+
+const defaultDifficulty = 4;
+const algorithms = {
+  fast: processFast,
+  slow: processSlow,
+};
+
+const status = document.getElementById("status");
+const difficultyInput = document.getElementById("difficulty-input");
+const algorithmSelect = document.getElementById("algorithm-select");
+const results = document.getElementById("results");
+
+const setupControls = () => {
+  difficultyInput.value = defaultDifficulty;
+  for (const alg of Object.keys(algorithms)) {
+    const option = document.createElement("option");
+    option.value = option.innerText = alg;
+    algorithmSelect.append(option);
+  }
+};
+
+const benchmarkTrial = async (stats, difficulty, algorithm) => {
+  if (!(difficulty >= 1)) {
+    throw new Error(`Invalid difficulty: ${difficulty}`);
+  }
+  const process = algorithms[algorithm];
+  if (process == null) {
+    throw new Error(`Unknown algorithm: ${algorithm}`);
+  }
+
+  const rawChallenge = new Uint8Array(32);
+  crypto.getRandomValues(rawChallenge);
+  const challenge = Array.from(rawChallenge)
+    .map((c) => c.toString(16).padStart(2, "0"))
+    .join("");
+
+  const t0 = performance.now();
+  const { hash, nonce } = await process(challenge, Number(difficulty));
+  const t1 = performance.now();
+  console.log({ hash, nonce });
+
+  stats.time += t1 - t0;
+  stats.iters += nonce;
+
+  return { time: t1 - t0, nonce };
+};
+
+const stats = { time: 0, iters: 0 };
+const updateStatus = () => {
+  const rate = stats.iters / stats.time;
+  if (Number.isFinite(rate)) {
+    status.innerText = `Average hashrate: ${rate.toFixed(3)}kH/s`;
+  } else {
+    status.innerText = "Benchmarking...";
+  }
+};
+
+const tableCell = (text) => {
+  const td = document.createElement("td");
+  td.innerText = text;
+  td.style.padding = "0 0.25rem";
+  return td;
+};
+
+const benchmarkLoop = async () => {
+  const difficulty = difficultyInput.value;
+  const algorithm = algorithmSelect.value;
+  updateStatus();
+
+  try {
+    const { time, nonce } = await benchmarkTrial(stats, difficulty, algorithm);
+
+    const tr = document.createElement("tr");
+    tr.style.display = "contents";
+    tr.append(tableCell(`${time}ms`), tableCell(nonce));
+
+    // auto-scroll to new rows
+    const atBottom =
+      results.scrollHeight - results.clientHeight <= results.scrollTop;
+    results.append(tr);
+    if (atBottom) {
+      results.scrollTop = results.scrollHeight - results.clientHeight;
+    }
+
+    updateStatus();
+  } catch (e) {
+    status.innerText = e;
+    return;
+  }
+
+  benchmarkLoop();
+};
+
+const reset = () => {
+  stats.time = stats.iters = 0;
+  results.innerHTML = status.innerText = "";
+
+  benchmarkLoop();
+};
+
+setupControls();
+difficultyInput.addEventListener("change", reset);
+algorithmSelect.addEventListener("change", reset);
+reset();
