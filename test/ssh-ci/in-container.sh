5bc1cf709279fe506ad3042d4194f3d2ec4ed7e0
Xe Iaso <me@xeiaso.net>
2025-06-11 06:03:41 -0400

    test(ssh-ci): speed up ci by prebaking the SSH CI image
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- test/ssh-ci/in-container.sh
+++ test/ssh-ci/in-container.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-apk add -U go nodejs git build-base git npm bash zstd brotli gzip
 npm ci
 npm run build
 SKIP_INTEGRATION=1 go test ./...
--- test/ssh-ci/rigging.sh
+++ test/ssh-ci/rigging.sh
@@ -7,14 +7,27 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: rigging.sh <user@host>"
 fi
 
+CIRunnerImage="ghcr.io/techarohq/anubis/ci-runner:latest"
 RunID=${GITHUB_RUN_ID:-$(uuidgen)}
 RunFolder="anubis/runs/${RunID}"
 Target="${1}"
 
 ssh "${Target}" uname -av
 ssh "${Target}" mkdir -p "${RunFolder}"
-ssh "${Target}" mkdir -p "anubis/cache/{go,go-build,node}"
 git archive HEAD | ssh "${Target}" tar xC "${RunFolder}"
-ssh "${Target}" podman pull alpine:3.22
-ssh "${Target}" podman run --rm -itv "\$HOME/${RunFolder}:/app/anubis" -v "\$HOME/anubis/cache/go:/root/go" -v "\$HOME/anubis/cache/go-build:/root/.cache/go-build" -v "\$HOME/anubis/cache/node:/root/.npm" -w /app/anubis alpine:3.22 sh /app/anubis/test/ssh-ci/in-container.sh
-ssh "${Target}" rm -rf "${RunFolder}"
+
+ssh "${Target}" << EOF
+  ssh -euo pipefail
+  set -x
+  mkdir -p "anubis/cache/{go,go-build,node}"
+  podman pull ${CIRunnerImage}
+  podman run --rm -it \
+    -v "\$HOME/${RunFolder}:/app/anubis" \
+    -v "\$HOME/anubis/cache/go:/root/go" \
+    -v "\$HOME/anubis/cache/go-build:/root/.cache/go-build" \
+    -v "\$HOME/anubis/cache/node:/root/.npm" \
+    -w /app/anubis \
+    ${CIRunnerImage} \
+    sh /app/anubis/test/ssh-ci/in-container.sh
+  ssh "${Target}" rm -rf "${RunFolder}"
+EOF
