bcbf377a8e3ced01290faae29d1e268af47b025d
Xe Iaso <me@xeiaso.net>
2025-06-27 13:10:38 -0400

    fix(challenge/metarefresh): enable localization
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- lib/challenge/metarefresh/metarefresh.go
+++ lib/challenge/metarefresh/metarefresh.go
@@ -34,9 +34,9 @@ func (i *Impl) Issue(r *http.Request, lg *slog.Logger, in *challenge.IssueInput)
 	q.Set("redir", r.URL.String())
 	q.Set("challenge", in.Challenge)
 	u.RawQuery = q.Encode()
-	
+
 	loc := localization.GetLocalizer(r)
-	component, err := web.BaseWithChallengeAndOGTags(loc.T("making_sure_not_bot"), page(in.Challenge, u.String(), in.Rule.Challenge.Difficulty), in.Impressum, in.Challenge, in.Rule.Challenge, in.OGTags, loc.Localizer)
+	component, err := web.BaseWithChallengeAndOGTags(loc.T("making_sure_not_bot"), page(in.Challenge, u.String(), in.Rule.Challenge.Difficulty, loc), in.Impressum, in.Challenge, in.Rule.Challenge, in.OGTags, loc.Localizer)
 
 	if err != nil {
 		return nil, fmt.Errorf("can't render page: %w", err)
