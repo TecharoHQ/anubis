TecharoHQ/anubis

commit 68b653b0998f03ff37fa53a3e5f7439f07617860

feat(anubis): add /healthz route to metrics server (#843)
* feat(anubis): add /healthz route to metrics server

Also add health check test for Docker Compose and update documentation
for health checking Anubis with Docker Compose.

Signed-off-by: Xe Iaso <me@xeiaso.net>

* chore: spelling

Signed-off-by: Xe Iaso <me@xeiaso.net>

---------

Signed-off-by: Xe Iaso <me@xeiaso.net>

cmd/anubis/main.go
@@ -38,6 +38,7 @@ import (
 	"github.com/facebookgo/flagenv"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	healthv1 "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 var (
@@ -97,7 +98,7 @@ func keyFromHex(value string) (ed25519.PrivateKey, error) {
 }
 
 func doHealthCheck() error {
-	resp, err := http.Get("http://localhost" + *metricsBind + anubis.BasePrefix + "/metrics")
+	resp, err := http.Get("http://localhost" + *metricsBind + "/healthz")
 	if err != nil {
 		return fmt.Errorf("failed to fetch metrics: %w", err)
 	}
@@ -241,6 +242,15 @@ func main() {
 	}
 
 	internal.InitSlog(*slogLevel)
+	internal.SetHealth("anubis", healthv1.HealthCheckResponse_NOT_SERVING)
+
+	if *healthcheck {
+		log.Println("running healthcheck")
+		if err := doHealthCheck(); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
 
 	if *extractResources != "" {
 		if err := extractEmbedFS(data.BotPolicies, ".", *extractResources); err != nil {
@@ -251,6 +261,14 @@ func main() {
 		}
 		fmt.Printf("Extracted embedded static files to %s\n", *extractResources)
 		return
+	}
+
+	ctx := context.Background()
+	wg := new(sync.WaitGroup)
+
+	if *metricsBind != "" {
+		wg.Add(1)
+		go metricsServer(ctx, wg.Done)
 	}
 
 	var rp http.Handler
@@ -266,8 +284,6 @@ func main() {
 	if *cookieDomain != "" && *cookieDynamicDomain {
 		log.Fatalf("you can't set COOKIE_DOMAIN and COOKIE_DYNAMIC_DOMAIN at the same time")
 	}
-
-	ctx := context.Background()
 
 	// Thoth configuration
 	switch {
@@ -398,15 +414,9 @@ func main() {
 		log.Fatalf("can't construct libanubis.Server: %v", err)
 	}
 
-	wg := new(sync.WaitGroup)
 	// install signal handler
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
-
-	if *metricsBind != "" {
-		wg.Add(1)
-		go metricsServer(ctx, wg.Done)
-	}
 
 	var h http.Handler
 	h = s
@@ -442,6 +452,8 @@ func main() {
 		}
 	}()
 
+	internal.SetHealth("anubis", healthv1.HealthCheckResponse_SERVING)
+
 	if err := srv.Serve(listener); !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal(err)
 	}
@@ -452,19 +464,29 @@ func metricsServer(ctx context.Context, done func()) {
 	defer done()
 
 	mux := http.NewServeMux()
-	mux.Handle(anubis.BasePrefix+"/metrics", promhttp.Handler())
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		st, ok := internal.GetHealth("anubis")
+		if !ok {
+			slog.Error("health service anubis does not exist, file a bug")
+		}
+
+		switch st {
+		case healthv1.HealthCheckResponse_NOT_SERVING:
+			http.Error(w, "NOT OK", http.StatusInternalServerError)
+			return
+		case healthv1.HealthCheckResponse_SERVING:
+			fmt.Fprintln(w, "OK")
+			return
+		default:
+			http.Error(w, "UNKNOWN", http.StatusFailedDependency)
+			return
+		}
+	})
 
 	srv := http.Server{Handler: mux, ErrorLog: internal.GetFilteredHTTPLogger()}
 	listener, metricsUrl := setupListener(*metricsBindNetwork, *metricsBind)
 	slog.Debug("listening for metrics", "url", metricsUrl)
-
-	if *healthcheck {
-		log.Println("running healthcheck")
-		if err := doHealthCheck(); err != nil {
-			log.Fatal(err)
-		}
-		return
-	}
 
 	go func() {
 		<-ctx.Done()

.github/workflows/smoke-tests.yml
@@ -10,7 +10,13 @@ permissions:
   contents: read
 
 jobs:
-  git-clone:
+  smoke-test:
+    strategy:
+      matrix:
+        test:
+          - git-clone
+          - git-push
+          - healthcheck
     runs-on: ubuntu-24.04
     steps:
       - name: Checkout code
@@ -30,28 +36,5 @@ jobs:
 
       - name: Run test
         run: |
-          cd test/git-clone
-          ./test.sh
-
-  git-push:
-    runs-on: ubuntu-24.04
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@11bd71901bbe5b1630ceea73d27597364c9af683 # v4.2.2
-        with:
-          persist-credentials: false
-
-      - uses: actions/setup-node@49933ea5288caeca8642d1e84afbd3f7d6820020 # v4.4.0
-        with:
-          node-version: latest
-
-      - uses: actions/setup-go@d35c59abb061a4a6fb18e82ac0862c26744d6ab5 # v5.5.0
-        with:
-          go-version: stable
-
-      - uses: ko-build/setup-ko@d006021bd0c28d1ce33a07e7943d48b079944c8d # v0.9
-
-      - name: Run test
-        run: |
-          cd test/git-push
+          cd test/${{ matrix.test }}
           ./test.sh

docs/docs/admin/environments/docker-compose.mdx
@@ -4,7 +4,7 @@ Docker compose is typically used in concert with other load balancers such as [A
 
 ```yaml
 services:
-  anubis-nginx:
+  anubis:
     image: ghcr.io/techarohq/anubis:latest
     environment:
       BIND: ":8080"
@@ -15,10 +15,17 @@ services:
       POLICY_FNAME: "/data/cfg/botPolicy.yaml"
       OG_PASSTHROUGH: "true"
       OG_EXPIRY_TIME: "24h"
+    healthcheck:
+      test: ["CMD", "anubis", "--healthcheck"]
+      interval: 5s
+      timeout: 30s
+      retries: 5
+      start_period: 500ms
     ports:
       - 8080:8080
     volumes:
       - "./botPolicy.yaml:/data/cfg/botPolicy.yaml:ro"
+
   nginx:
     image: nginx
     volumes:

docs/manifest/deployment.yaml
@@ -36,6 +36,18 @@ spec:
               mountPath: /conf
           ports:
             - containerPort: 80
+          readinessProbe:
+            httpGet:
+              path: /
+              port: 80
+            initialDelaySeconds: 1
+            periodSeconds: 10
+          livenessProbe:
+            httpGet:
+              path: /
+              port: 80
+            initialDelaySeconds: 10
+            periodSeconds: 20
         - name: anubis
           image: ghcr.io/techarohq/anubis:main
           imagePullPolicy: Always
@@ -80,4 +92,15 @@ spec:
               type: RuntimeDefault
           envFrom:
             - secretRef:
-                name: anubis-docs-thoth
+          readinessProbe:
+            httpGet:
+              path: /healthz
+              port: 9090
+            initialDelaySeconds: 1
+            periodSeconds: 10
+          livenessProbe:
+            httpGet:
+              path: /healthz
+              port: 9090
+            initialDelaySeconds: 10
+            periodSeconds: 20

internal/health.go
@@ -1,0 +1,25 @@
+package internal
+
+import (
+	"context"
+
+	"google.golang.org/grpc/health"
+	healthv1 "google.golang.org/grpc/health/grpc_health_v1"
+)
+
+var HealthSrv = health.NewServer()
+
+func SetHealth(svc string, status healthv1.HealthCheckResponse_ServingStatus) {
+	HealthSrv.SetServingStatus(svc, status)
+}
+
+func GetHealth(svc string) (healthv1.HealthCheckResponse_ServingStatus, bool) {
+	st, err := HealthSrv.Check(context.Background(), &healthv1.HealthCheckRequest{
+		Service: svc,
+	})
+	if err != nil {
+		return healthv1.HealthCheckResponse_UNKNOWN, false
+	}
+
+	return st.GetStatus(), true
+}

test/git-clone/docker-compose.yaml
@@ -12,11 +12,11 @@ services:
   anubis:
     image: ko.local/anubis
     environment:
-      BIND: ":3000"
+      BIND: ":8005"
       TARGET: http://cgit:80
       USE_REMOTE_ADDRESS: "true"
     ports:
-      - 3000:3000
+      - 8005:8005
 
 volumes:
   cgit-data:

test/git-clone/test.sh
@@ -21,6 +21,6 @@ docker compose up -d
 
 sleep 2
 
-(cd ./var/clones && git clone http://localhost:3000/status.git)
+(cd ./var/clones && git clone http://localhost:8005/status.git)
 
 docker compose down

test/healthcheck/docker-compose.yaml
@@ -1,0 +1,15 @@
+services:
+  web:
+    image: ghcr.io/xe/nginx-micro:v1.29.0
+
+  anubis:
+    image: ko.local/anubis
+    environment:
+      TARGET: http://web:80
+      USE_REMOTE_ADDRESS: "true"
+    healthcheck:
+      test: ["CMD", "anubis", "--healthcheck"]
+      interval: 5s
+      timeout: 30s
+      retries: 5
+      start_period: 500ms

commit 3907122a213c2524d90fd9fb38bccf8a2549bce0

fix(cmd/anubis): add signal handling to metrics server
This fixes a bug that was introduced in 68b653b0, in which the call
to metricsServer was passed a plain context.Background without
signal handling.

This commit adds back in the signal handling for the metrics server,
as well as for the Thoth client and storage backend.

Closes: #853

Signed-off-by: Emily Rowlands <emily@erowl.net>

cmd/anubis/main.go
@@ -263,7 +263,10 @@ func main() {
 		return
 	}
 
-	ctx := context.Background()
+	// install signal handler
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	wg := new(sync.WaitGroup)
 
 	if *metricsBind != "" {
@@ -413,10 +416,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("can't construct libanubis.Server: %v", err)
 	}
-
-	// install signal handler
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
 
 	var h http.Handler
 	h = s