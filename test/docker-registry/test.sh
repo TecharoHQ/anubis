2ab4e5546fc94c9a2fb7a627e20d8974d579d40d
Xe Iaso <me@xeiaso.net>
2025-11-07 18:47:39 -0500

    fix(data): add ruleset to explicitly allow Docker / OCI clients
    Fixes #1252
    
    This is technically a regression as these clients used to work in Anubis
    v1.22.0, however it is allowable to make this opt-in as most websites do not
    expect to be serving Docker / OCI registry client traffic.
    
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ test/docker-registry/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source ../lib/lib.sh
+
+build_anubis_ko
+
+function cleanup() {
+	docker compose down
+}
+
+trap cleanup EXIT SIGINT
+
+mint_cert registry.local.cetacean.club
+
+docker compose up -d
+
+backoff-retry skopeo \
+	--insecure-policy \
+	copy \
+	--dest-tls-verify=false \
+	docker://hello-world \
+	docker://registry.local.cetacean.club:3004/hello-world
