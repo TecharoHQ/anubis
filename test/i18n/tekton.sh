03bf695dffa2d9582e0929a36e38b5ebc22d4600
Xe Iaso <me@xeiaso.net>
2026-05-16 03:07:29 -0400

    test: add i18n to tekton pipeline
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ test/i18n/tekton.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function cleanup() {
+	pkill -P $$
+}
+
+trap cleanup EXIT SIGINT
+
+go tool anubis --help 2>/dev/null || :
+
+go run ../cmd/unixhttpd &
+
+go tool anubis \
+	--policy-fname ./anubis.yaml \
+	--use-remote-address \
+	--target=unix://$(pwd)/unixhttpd.sock &
+
+backoff-retry node ./test.mjs
